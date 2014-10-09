da5791eef44f9c8e9ab5fe20fc87ff0c353e47f7
mx262cal726 <kylejrouse726@gmail.com>
2014-10-09 01:16:38 -0700

    yo

---
+++ yo.java
@@ -0,0 +1,25 @@
+
+
+
+
+public class yo
+{
+
+  public static void main(String args[])
+  {
+    String x = System.in;
+    String y = System.in;
+    int num = x.parseInt();
+    int num1 = y.parseInt();
+    add(num,num1);
+
+
+  }
+   
+   public int add(	int x, int yo)
+   {
+
+    return x +y;
+   }
+
+}
